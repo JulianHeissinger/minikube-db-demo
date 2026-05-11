9df22fec1250444f5142e053f0193a8d4471177b
JulianHeissinger <julian.heissinger01@gmail.com>
2026-05-11 16:17:03 +0200

    initial commit

---
+++ import.sql
@@ -0,0 +1,8 @@
+create table vehicle(
+    brand varchar(200),
+    model varchar(200)
+);
+
+INSERT INTO vehicle (brand, model) VALUES ('Opel', 'Kadett');
+INSERT INTO vehicle (brand, model) VALUES ('VW', 'Käfer 1400');
+INSERT INTO vehicle (brand, model) VALUES ('Opel', 'Blitz');
